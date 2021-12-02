6dae362e8d1f7b743dbb17ba71352ce65b5e4788
Carl Mattatall <carl.mattatall@rimot.io>
2021-12-02 16:55:50 -0400

    Initial commit

---
+++ q2/q2.cpp
@@ -0,0 +1,50 @@
+#include <fstream>
+#include <iostream>
+
+
+
+class Solution(){
+  public:
+    Solution() : input("input.txt"){
+
+  }
+  
+    ~Solution() {
+    input.close();
+  }
+
+  void part1();
+  void part2();
+  private:
+    std::istream input;
+};
+
+
+void Solution::part1(){
+
+
+
+}
+
+
+void Solution::part2(){
+
+
+
+
+}
+
+
+int main(int argc, char**argv){
+  
+  Solution solution;
+
+
+  solution.part1();
+
+
+  solution.part2();
+
+
+  return 0;
+}
