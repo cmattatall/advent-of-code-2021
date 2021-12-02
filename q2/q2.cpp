1ea708db2b538c53f02f59f1a7cbd7fa6dd667cb
Carl Mattatall <carl.mattatall@rimot.io>
2021-12-02 17:57:44 -0400

    [Add] solution for part2

--- q2/q2.cpp
+++ q2/q2.cpp
@@ -4,13 +4,15 @@
 #include <vector>
 #include <sstream>
 #include <charconv>
+#include <functional>
 
 struct point {
     int x;
     int y;
+    int aim;
 };
 
-class Solution {
+struct Solution {
    public:
     Solution() : input("input.txt"), coords{0, 0} {}
 
@@ -21,10 +23,8 @@ class Solution {
     void part1();
     void part2();
 
-   private:
     void init();   // rewind the input file for reading and reset the coordinates
     void print();  // output the coordinates
-    void answer();
     std::vector<std::string> tokenize(std::string const &line);
     std::ifstream input;
 
@@ -45,10 +45,6 @@ void Solution::print() {
     std::cout << "(" << coords.x << ", " << coords.y << ")" << std::endl;
 }
 
-void Solution::answer() {
-    std::cout << coords.x * coords.y << std::endl;
-}
-
 enum class Direction { DIR_X, DIR_Y };
 
 struct Dirmap {
@@ -56,16 +52,17 @@ struct Dirmap {
     int sign;
 };
 
-std::map<std::string, Dirmap> dirmap = {{"forward", {Direction::DIR_X, 1}},
-                                        {"backward", {Direction::DIR_X, -1}},
-                                        {"up", {Direction::DIR_Y, -1}},    // we are measuring DEPTH so UP is negative
-                                        {"down", {Direction::DIR_Y, 1}}};  // we are measuring DEPTH so DOWN is positive
+std::map<std::string, Dirmap> dirmap1 = {
+    {"forward", {Direction::DIR_X, 1}},
+    {"up", {Direction::DIR_Y, -1}},    // we are measuring DEPTH so UP is negative
+    {"down", {Direction::DIR_Y, 1}}};  // we are measuring DEPTH so DOWN is positive
 
 void Solution::init() {
     input.clear();
     input.seekg(0);
     coords.x = 0;
     coords.y = 0;
+    coords.aim = 0;
 }
 
 void Solution::part1() {
@@ -78,26 +75,63 @@ void Solution::part1() {
         std::string const &dist_str = fields[1];
         std::from_chars(dist_str.data(), dist_str.data() + dist_str.size(), distance);
 
-        switch (dirmap[fields[0]].dir) {
+        switch (dirmap1[fields[0]].dir) {
             case Direction::DIR_X:
-                coords.x += dirmap[fields[0]].sign * distance;
+                coords.x += dirmap1[fields[0]].sign * distance;
                 break;
             case Direction::DIR_Y:
-                coords.y += dirmap[fields[0]].sign * distance;
+                coords.y += dirmap1[fields[0]].sign * distance;
                 break;
             default:
                 throw std::string("THERES A BUG IN YOUR CODE");
         }
     }
-    answer();
+    std::cout << coords.x * coords.y << std::endl;
 }
 
 void Solution::part2() {
     init();
+    std::map<std::string, std::function<void(std::string const &)>> dirmap2 = {
+        {"forward",
+         [this](std::string const &dist_str) {
+             int value;
+             std::from_chars(dist_str.data(), dist_str.data() + dist_str.size(), value);
+
+             std::cout << "before >forward " << dist_str << "< , x = " << this->coords.x << ", y = " << this->coords.y
+                       << " and aim = " << this->coords.aim << std::endl;
+             this->coords.x += value;
+             this->coords.y += value * this->coords.aim;
+             std::cout << "after >forward " << dist_str << "< , x = " << this->coords.x << ", y = " << this->coords.y
+                       << " and aim = " << this->coords.aim << std::endl;
+         }},
+        {"up",
+         [this](std::string const &dist_str) {
+             int value;
+             std::from_chars(dist_str.data(), dist_str.data() + dist_str.size(), value);
+
+             std::cout << "before >up " << dist_str << "< , x = " << this->coords.x << ", y = " << this->coords.y
+                       << " and aim = " << this->coords.aim << std::endl;
+             this->coords.aim -= value;
+             std::cout << "after >up " << dist_str << "< , x = " << this->coords.x << ", y = " << this->coords.y
+                       << " and aim = " << this->coords.aim << std::endl;
+         }},
+        {"down", [this](std::string const &dist_str) {
+             int value;
+             std::from_chars(dist_str.data(), dist_str.data() + dist_str.size(), value);
+             std::cout << "before >down " << dist_str << "< , x = " << this->coords.x << ", y = " << this->coords.y
+                       << " and aim = " << this->coords.aim << std::endl;
+             this->coords.aim += value;
+             std::cout << "after >down " << dist_str << "< , x = " << this->coords.x << ", y = " << this->coords.y
+                       << " and aim = " << this->coords.aim << std::endl;
+         }}};
+
     std::string line;
     while (std::getline(input, line)) {
+        std::cout << "line = " << line << std::endl;
+        std::vector<std::string> fields = tokenize(line);
+        dirmap2[fields[0]](fields[1]);
     }
-    answer();
+    std::cout << coords.x * coords.y << std::endl;
 }
 
 int main(int argc, char **argv) {
